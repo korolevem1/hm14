3199521e770a4170a762e1deb3f9d16ae5f83f01
Eduard <korolev.e@inbox.ru>
2021-07-08 01:40:34 +0300

    release 08.07.21 make ubuntu server in google cloud

---
+++ config.tf
@@ -0,0 +1,42 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "3.74.0"
+    }
+  }
+}
+
+provider "google" {
+  # Configuration options
+project = "micro-shoreline-313408"
+region = "us-central1"
+}
+
+
+resource "google_compute_instance" "default" {
+  name         = "test"
+  machine_type = "e2-medium"
+  zone         = "us-central1-a"
+
+  tags = ["foo", "bar"]
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-2004-focal-v20210623"
+    }
+  }
+
+
+
+network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemeral IP
+    }
+}
+
+
+
+}
