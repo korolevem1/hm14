059068bd5c401268c473d1d2e09acfe98948302b
Eduard <korolev.e@inbox.ru>
2021-07-08 13:16:35 +0300

    release 1.0. hm 14 part1

--- config-part1.tf
+++ config-part1.tf
@@ -44,3 +44,20 @@ network_interface {
   provisioner "local-exec" {
     command = "gcloud compute ssh test4 --zone=us-central1-a --ssh-key-file ~/.ssh/id_rsa  --command=\"apt install nginx -y\""
   }
+
+  provisioner "file" {
+    source = "/root/terraform1/index.html"
+    destination = "/var/www/html/index.nginx-debian.html"
+
+connection {
+user = "root"
+type = "ssh"
+private_key= "${file("/root/.ssh/google_compute_engine")}"
+host = "${google_compute_instance.default.network_interface.0.access_config.0.nat_ip}"
+agent = false
+timeout = "30s"
+}
+  
+
+}
+
